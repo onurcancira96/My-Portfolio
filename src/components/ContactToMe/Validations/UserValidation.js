83ed89183890fd0daada2442d79995a0e7d05b0c
onurcancira96 <onurcan.cira96@gmail.com>
2023-08-31 23:01:24 +0300

    Updates

--- src/components/ContactToMe/Validations/UserValidation.js
+++
@@ -1,8 +0,0 @@
-import * as yup from 'yup';
-
-
-export const contactFormSchema = yup.object().shape({
-    user_name: yup.string().required('❌ Name is required.').min(3,'❌ Minimum 3 characters.').max(25,'❌ Maximum 1000 characters'),
-    user_email: yup.string().email('❌ Invalid email address.').required('❌ Email is required.'),
-    message: yup.string().min(3,'❌ Minimum 3 characters').max(1000,'❌ Maximum 1000 characters').required('❌ Message is required.'),
-});
